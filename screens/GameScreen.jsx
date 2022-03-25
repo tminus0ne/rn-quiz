c88e8e9734d07e5809e87771cda7c95e97221e3f
Greg-One <sgmdoc@yandex.ru>
2022-03-25 15:48:43 +0500

    add app screen templates

---
+++ screens/GameScreen.jsx
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+const GameScreen = () => {
+  return <View></View>;
+};
+
+export default GameScreen;
---
+++ screens/StartGameScreen.jsx
@@ -0,0 +1,11 @@
+import { View, TextInput } from 'react-native';
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+};
+
+export default StartGameScreen;
