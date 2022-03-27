725123cfc0d793e75d712a53c94499d52a740bc4
Greg-One <sgmdoc@yandex.ru>
2022-03-27 20:08:55 +0500

    add app background styling

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient style={styles.rootScreen} colors={['#4e0329', '#ddb52f']}>
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode="cover"
+        imageStyle={styles.backgorundImage}
+        style={styles.rootScreen}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgorundImage: {
+    opacity: 0.2,
+  },
+});
