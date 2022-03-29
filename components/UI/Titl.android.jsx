a04d88319cf9df280aa99006aed4337419a5dd61
Greg-One <sgmdoc@yandex.ru>
2022-03-29 23:04:27 +0500

    add platform specific files

--- components/UI/Titl.android.jsx
+++ components/UI/Titl.android.jsx
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, Platform } from 'react-native';
 
 const Title = ({ children }) => {
   return <Text style={styles.title}>{children}</Text>;
@@ -10,6 +10,11 @@ const styles = StyleSheet.create({
     fontSize: 24,
     color: 'white',
     textAlign: 'center',
+    // borderWidth: Platform.OS === 'android' ? 2 : 0 ,
+    // borderWidth: Platform.select({
+    //   ios: 0,
+    //   android: 2,
+    // }),
     borderWidth: 2,
     borderColor: 'white',
     padding: 12,
---
+++ components/UI/Title.ios.jsx
@@ -0,0 +1,23 @@
+import { Text, StyleSheet, Platform } from 'react-native';
+
+const Title = ({ children }) => {
+  return <Text style={styles.title}>{children}</Text>;
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 24,
+    color: 'white',
+    textAlign: 'center',
+    // borderWidth: Platform.OS === 'android' ? 2 : 0 ,
+    // borderWidth: Platform.select({
+    //   ios: 0, 
+    // }),
+    padding: 12,
+    maxWidth: '80%',
+    width: 300,
+  },
+});
+
+export default Title;
